2a82a90a0ae9649b017dcaed6dfd9ff02cf9ec6b
Rasik <rasikraj01@gmail.com>
2017-08-26 09:30:14 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,19 +1,31 @@
 var x = mydata.data[0];
+console.log(x[10]);
 
   function showinrange(a,b) {
          //console.log("in range");
-
-         var i=0;
-         var ctr=0;
-         for(i=0;i<10000;i++) {
-
          cr1=0;cr2=0;cr3=0;cr4=0;cr5=0;
          i=0;
          ctr=0;
          for(i=0;i<1000;i++) {
+            var x = mydata.data[i];
+            dist = getDistanceFromLatLonInKm(a,b,x[22],x[23]);
+            if(dist<5)
+               {
+                   console.log(x[10]);
+                   ctr=1;
+               }
+             if(x[12] == "THEFT")
+                 cr1++;
+             if(x[12] == "DECEPTIVE PRACTICE")
+                 cr2++;
+             if(x[12] == "BURGLARY")
+                 cr3++;
+             if(x[12] == "CRIMINAL TRESPASS")
+                 cr4++;
+             if(x[12] == "ASSAULT")
+                 cr5++;
 
-             
-             
+
         }
          if(ctr==0)
                  console.log("none");
@@ -43,7 +55,7 @@ var targetLL;
          map.addListener('click', function(e) {
           map.panTo(e.latLng);
           targetLL = e.latLng;
-
+         console.log(targetLL.lat());
         showinrange(targetLL.lat() , targetLL.lng());
 
 
@@ -51,9 +63,9 @@ var targetLL;
         geocoder.geocode({'location': targetLL}, function(results, status){
           if (status === 'OK') {
           if (results[0]) {
-              
-              loc = results[0].address_components[2].long_name;
-              $.get("http://webhose.io/filterWebContent?token=05e31117-ac3c-4128-800f-79b98b58af9f&format=json&sort=crawled&q=%22"+loc+"%22(title%3A'murder')%20language%3Aenglish%20site_type%3Anews" , function(data) {
+            loc = results[0].formatted_address;
+            console.log(loc + "locatop");
+              $.get("http://webhose.io/filterWebContent?token=05e31117-ac3c-4128-800f-79b98b58af9f&format=json&sort=crawled&q=%22chicago%22(title%3A'murder')%20language%3Aenglish%20site_type%3Anews" , function(data) {
                   console.log("found " + data.posts.length + " rows");
                   $('#count').append("<p>Number of crimes reported nearby in the last 7 days = "+data.posts.length + " !</p>");
               });
@@ -100,15 +112,3 @@ function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
         }
                })
            });
-
-
-
-         //   $(document).ready(function() {
-           //
-         //      setTimeout(function(){
-         //         $('body').addClass('loaded');
-         //         $('h1').css('color','#222222');
-         //      }, 3000);
-           //
-         //   });
-
